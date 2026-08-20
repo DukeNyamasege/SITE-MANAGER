8f1ea25c23224d1125b5e54bdf319656d7fb172f
Mr Duke <133217491+DukeNyamasege@users.noreply.github.com>
2026-08-20 14:55:33 +0300

    feat: put Site Manager into maintenance mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppV2';
-import './styles.css';
-import './customization.css';
-import './netlify-only.css';
+import MaintenancePage from './MaintenancePage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MaintenancePage />
   </StrictMode>
 );
